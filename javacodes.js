e253df8d307aa09a3af0d0597285a5973c72ffc9
Wecode <mashleyalpha@gmail.com>
2018-12-14 09:20:29 +0200

    added something

--- javacodes.js
+++ javacodes.js
@@ -1,31 +1,41 @@
-var triangle = function(sideOne, sideTwo, sideThree){
-    if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)) {
-      if (sideOne===sideTwo && sideTwo===sideThree) {
-        return "equilateral";
-      } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
-        return "isosceles";
-      } else {
-        return "scalene";
-      }
-    } else {
-      return "These sides don't make a triangle";
-    }
+var sideSum1 = function(firstvalue, secondvalue) {
+    return firstvalue + secondvalue;
   };
   
+  var sideSum2 = function(secondvalue, thirdvalue) {
+    return secondvalue + thirdvalue;
+  };
   
-  $(document).ready(function() {
-    $("form#triangle").submit(function(event) {
+  var sideSum3 = function(firstvalue, thirdvalue) {
+    return firstvalue + thirdvalue;
+  };
+  function check(){
   
-      var sideOne = parseInt($("input#sideOne").val());
-      var sideTwo = parseInt($("input#sideTwo").val());
-      var sideThree = parseInt($("input#sideThree").val());
-      var result = triangle(sideOne, sideTwo, sideThree);
+     var firstvalue=document.getElementById('one').value;
+     var secondvalue=document.getElementById('two').value;
+     var thirdvalue=document.getElementById('three').value;
+     
+     var text;
+      if(sideSum1(firstvalue,secondvalue) <= thirdvalue || sideSum2(secondvalue,thirdvalue) <= firstvalue || sideSum3(firstvalue,thirdvalue) <= secondvalue){
+         text="it is not a triangle"
+     }
+     else if(firstvalue === secondvalue && secondvalue === thirdvalue){
+         text = "Equilaterial Triangle.";
+     }
+      //Equivalent Triangle
   
-      sideOne = parseInt($("input#sideOne").val(""));
-      sideTwo = parseInt($("input#sideTwo").val(""));
-      sideThree = parseInt($("input#sideThree").val(""));
+     else if(firstvalue===secondvalue || firstvalue===thirdvalue || secondvalue===thirdvalue) {
+         text="Isoscele Triangle.";
+     }
+     //Isoscele Triangle
+     else if(firstvalue!==secondvalue && firstvalue!==thirdvalue &&secondvalue!==thirdvalue){
+         text="Scalene Triangle."
+     }
+     //Scalene Triangle
   
-      $("#result").append(result);
-      event.preventDefault();
-    });
-  });
+  
+     else{
+         text = "Not a Triangle.";
+     }
+     document.getElementById('demo').innerHTML = text;
+  }
